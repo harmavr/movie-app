b20b58d15ca476ce3f256a72e6d10aa4eb29b058
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-06-27 12:43:30 +0300

    functionality of the slices for the redux

---
+++ src/redux/MovieSlice.jsx
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  selectedMovie: null,
+  isModalOpen: false,
+  collectionList: [],
+};
+
+const movieSlice = createSlice({
+  name: "movie",
+  initialState,
+  reducers: {
+    setMovies(state, action) {
+      const { collectionId, movie } = action.payload;
+      const collection = state.collectionList.find(
+        (col) => col.id === parseInt(collectionId)
+      );
+      if (collection) {
+        collection.movies.push(movie);
+      }
+    },
+    setMovieDetails(state, action) {
+      state.selectedMovie = action.payload;
+      state.isModalOpen = true;
+    },
+    setSelectedMovie(state, action) {
+      state.selectedMovie = action.payload;
+      state.isModalOpen = false;
+    },
+    closeModal(state) {
+      state.isModalOpen = false;
+      state.selectedMovie = null;
+    },
+    saveCollection(state, action) {
+      const newCollection = {
+        id: state.collectionList.length + 1,
+        name: action.payload.title,
+        description: action.payload.description,
+        movies: [],
+      };
+      state.collectionList.push(newCollection);
+    },
+  },
+});
+
+export const movieActions = movieSlice.actions;
+export default movieSlice.reducer;
