b5034819624b9adce5be7745661692885cb60aa0
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-07-12 14:06:37 +0300

    style Collections page

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./Collections.css";
 
 export default function Collections() {
   const [collectionList, setCollectionList] = useState([]);
