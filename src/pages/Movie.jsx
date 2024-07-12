0ef2c1003820e41469825071b926c3380dc3ebcf
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-07-12 14:07:16 +0300

    style Movie page

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { movieActions } from "../redux/MovieSlice";
 import Modal from "../components/Modal";
 import { Rating, Typography } from "@mui/material";
+import "./Movie.css";
 
 const Movie = () => {
   const { id } = useParams();
